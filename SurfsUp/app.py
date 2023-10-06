d47af41e398b681548cb24fd6d6a0c1f9421bba8
snethomas <snethomas@gmail.com>
2023-10-06 15:48:32 -0700

    app.py update

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,7 +1,5 @@
 # Import the dependencies.
-
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -54,13 +52,13 @@ def precipitation():
     session = Session(engine)
     prcp_yr=session.query(measurement.date,measurement.prcp).filter(measurement.date>='2016-08-23').all()
     session.close()
-    prcp_yr_list = [{"Date": p[0], "Precipitation": p[1]} for p in prcp_yr]
+    prcp_yr_list = [{p[0]: p[1]} for p in prcp_yr]
     return jsonify(prcp_yr_list)
 
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
-    stations=session.query(station).all()
+    stations=session.query(station.station,station.name,station.latitude,station.longitude,station.elevation).all()
     session.close()
     stations_list = [{"station": s[0], "name": s[1],"latitude": s[2], "longitude": s[3], "elevation": s[4]} for s in stations]
     return jsonify(stations_list)
@@ -68,10 +66,10 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
     session = Session(engine)
-    most_active_temp=session.query(measurement.tobs).filter(measurement.station=='USC00519281')\
+    most_active_temp=session.query(measurement.date,measurement.tobs).filter(measurement.station=='USC00519281')\
         .filter(measurement.date>='2016-08-18').all()
     session.close()
-    most_active_temp_list = [{"Temperature": t[0]} for t in most_active_temp]
+    most_active_temp_list = [{t[0]:t[1]} for t in most_active_temp]
     return jsonify(most_active_temp_list)
 
 @app.route("/api/v1.0/<start>")
@@ -84,7 +82,7 @@ def start(start_date):
     return jsonify(temp_range_list)
 
 @app.route("/api/v1.0/<start>/<end>")
-def start(start_date,end_date):
+def start_end(start_date,end_date):
     session = Session(engine)
     temp_range=session.query(func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs))\
     .filter(measurement.date>=start_date,measurement.date<=end_date ).all()
